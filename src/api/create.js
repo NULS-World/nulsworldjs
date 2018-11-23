68f4fb8860db9cdefea0a478ed697f2408e383be
Moshe Malawach <moshe.malawach@protonmail.com>
2018-11-23 14:54:29 +1100

    adding ability to switch server

--- src/api/create.js
+++ src/api/create.js
@@ -3,14 +3,17 @@ import { hash_from_address } from '../model/data'
 import {
   CHEAP_UNIT_FEE,
   Transaction, Coin} from '../model/transaction'
+import {DEFAULT_SERVER} from './base'
 
-export async function get_outputs (address) {
-  let response = await axios.get(`/addresses/outputs/${address}.json`)
+export async function get_outputs (address,
+                                   {api_server = DEFAULT_SERVER} = {}) {
+  let response = await axios.get(`${api_server}/addresses/outputs/${address}.json`)
   return response.data
 }
 
-export async function ipfs_push (value) {
-  let response = await axios.post('/ipfs/add_json', value)
+export async function ipfs_push (value,
+                                 {api_server = DEFAULT_SERVER} = {}) {
+  let response = await axios.post('${api_server}/ipfs/add_json', value)
   if (response.data.hash !== undefined) {
     return response.data.hash
   } else {
@@ -18,11 +21,12 @@ export async function ipfs_push (value) {
   }
 }
 
-export async function ipfs_push_file (fileobject) {
+export async function ipfs_push_file (fileobject,
+                                      {api_server = DEFAULT_SERVER} = {}) {
   let formData = new FormData();
   formData.append('file', fileobject);
 
-  let response = await axios.post( '/ipfs/add_file',
+  let response = await axios.post( '${api_server}/ipfs/add_file',
     formData,
     {
       headers: {
@@ -38,8 +42,9 @@ export async function ipfs_push_file (fileobject) {
   }
 }
 
-export async function prepare_remark_tx (address, remark) {
-  let outputs_data = await get_outputs(address)
+export async function prepare_remark_tx (address, remark,
+                                         {api_server = DEFAULT_SERVER} = {}) {
+  let outputs_data = await get_outputs(address, {'api_server': api_server})
 
   let tx = Transaction.from_dict(
     {'inputs': [
@@ -97,8 +102,9 @@ export async function create_post (address, post_type, content, title = null, re
   return tx
 }
 
-export async function broadcast (tx) {
-  let response = await axios.post('/broadcast', {
+export async function broadcast (tx,
+                                 {api_server = DEFAULT_SERVER} = {}) {
+  let response = await axios.post('${api_server}/broadcast', {
     txHex: this.signed_tx
   })
   return response.data.value;
