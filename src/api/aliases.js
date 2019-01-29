accaff4613550bd0f5e821281ab1ece584e8788d
Moshe Malawach <moshe.malawach@protonmail.com>
2019-01-29 18:16:59 +0100

    fix aliases address

--- src/api/aliases.js
+++ src/api/aliases.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {DEFAULT_SERVER} from './base'
 
 export async function get_aliases({api_server = DEFAULT_SERVER} = {}) {
-  let response = await axios(`${api_server}addresses/aliases/all.json`)
+  let response = await axios(`${api_server}/addresses/aliases/all.json`)
   if ((response.data !== undefined) && (response.data.aliases !== undefined))
     return response.data.aliases
 }
